7b722387686b48f3627b228909fee233d25c1465
Francisco Leví <francisco.levi.md@gmail.com>
2022-08-17 16:47:41 -0500

    Point3D class has been modified.

--- src/point_3d.cc
+++ src/point_3d.cc
@@ -1,9 +1,35 @@
- #include "point_3d.h"
+#include "point_3d.h"
 
- Point3D::Point3D(float x, float y, float z) : Point(x, y), z_(z) {}
+using namespace std;
 
- Point3D::~Point3D() {}
+ostream& operator<<(std::ostream& os, const Point3D& point) {
+    return os << "(" << point.x_ << ", " << point.y_ << ", " << point.z_ << ")";
+}
 
- void Point3D::set_z(float z) { 
+Point3D::Point3D(const Point3D& p) {
+    operator=(p);
+}
+
+Point3D::Point3D(const Point& p) {
+    operator=(p);
+}
+
+Point3D::Point3D(float x, float y, float z) : Point(x, y), z_(z) {}
+
+Point3D::~Point3D() {}
+
+void Point3D::set_z(float z) { 
     z_ = z; 
+}
+
+Point3D& Point3D::operator=(const Point3D& p) {
+    Point::operator=(p);
+    z_ = p.z_;
+    return *this;
+}
+
+Point3D& Point3D::operator=(const Point& p) {
+    Point::operator=(p);
+    z_ = 0.0f;
+    return *this;
 }
